5314864c8275f3a8a8f6adef60aee68a6585e595
Manase44 <fikopersempre@gmail.com>
2024-05-08 16:50:43 +0300

    initialize the project

---
+++ eticket-frontend/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='container'>
+        <div className='row h-100 align-items-center justify-contents-center'>
+            <img src="/Bus/Bus.png" className='img-fluid' alt="A bus picture" id='bus'/>
+            {/* <span>eTicket</span> */}
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
