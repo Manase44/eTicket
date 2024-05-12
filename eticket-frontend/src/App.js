3c1f7dab8fe0904a1fafa6ddd5bc758bf64bf5f3
Manase44 <fikopersempre@gail.com>
2024-05-12 18:39:52 +0300

    handle the routing

--- eticket-frontend/src/App.js
+++ eticket-frontend/src/App.js
@@ -4,12 +4,23 @@ import HomePage from './components/HomePage';
 import Reciept from './components/Reciept';
 import PaymentPage from './components/PaymentPage';
 import ChooseSeat from './components/ChooseSeat';
+import Thankyou from './components/Thankyou';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <ChooseSeat/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/book' element={<BookingPage/>}/>
+          <Route path='/choose-seat' element={<ChooseSeat/>}/>
+          <Route path='/pay' element={ <PaymentPage/>}/>
+          <Route path='/receipt' element={<Reciept/>}/>
+          <Route path='/thanks'element={<Thankyou/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
